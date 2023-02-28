fbdea5947ea1058415d26aa2212cd0603185270e
AlbaMSanchez <albasompadel@gmail.com>
2023-02-28 20:59:36 +0000

    carta finalizada con contador

--- src/app.js
+++ src/app.js
@@ -2,13 +2,118 @@
 import "bootstrap";
 import "./style.css";
 
-function show() {
-  // declarar carta inicial
-  const card ={
-    symbol:null; value:null
-  }
-  // obtener numero aleatoriamente
-  // obtener el palo aleatoriamente
-  // cambiar el estado de tarjeta
-  // mostrar en el html
-}
+// function show() {
+//   // declarar carta inicial
+//   const card ={
+//     symbol:null; value:null
+//   }
+//   // obtener numero aleatoriamente
+//   // obtener el palo aleatoriamente
+//   // cambiar el estado de tarjeta
+//   // mostrar en el html
+// }
+
+window.onload = function() {
+  //write your code here
+  let numbersCards = [
+    "AS",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let familyCards = [
+    {
+      name: "♦",
+      color: "rgb(130, 0, 0)"
+    },
+    {
+      name: "♥",
+      color: "rgb(130, 0, 0)"
+    },
+    {
+      name: "♠",
+      color: "black"
+    },
+    {
+      name: "♣",
+      color: "black"
+    }
+  ];
+
+  let positionRandom = nummax => {
+    const num = Math.floor(Math.random() * nummax);
+    return num;
+  };
+
+  let cardGenerator = () => {
+    let card = {
+      color: "",
+      number: 0,
+      family: ""
+    };
+    const positionRandomFamily = positionRandom(familyCards.length);
+    card.color = familyCards[positionRandomFamily].color;
+    card.family = familyCards[positionRandomFamily].name;
+    card.number = numbersCards[positionRandom(numbersCards.length)];
+    return card;
+  };
+
+  const update = () => {
+    let cardSelected = cardGenerator();
+    let familyCardRandom = document.querySelectorAll(".family");
+
+    for (let i = 0; i < familyCardRandom.length; i++) {
+      familyCardRandom[i].style.color = cardSelected.color;
+
+      familyCardRandom[i].innerHTML = cardSelected.family;
+    }
+
+    let numCardRandom = document.querySelector(".num-card");
+
+    if (cardSelected.number === "AS") {
+      const familyAs = cardSelected.family;
+      cardSelected.number = familyAs;
+    }
+
+    numCardRandom.style.color = cardSelected.color;
+
+    numCardRandom.innerHTML = cardSelected.number;
+  };
+
+  document.getElementById("click").addEventListener("click", update);
+
+  update();
+
+  let counter = 10;
+  let counterElement = document.querySelector(".counter");
+  const updateCounter = () => {
+    if (counter < 0) {
+      counter = 10;
+      update();
+    }
+    counterElement.innerHTML = counter;
+    counter--;
+  };
+  setInterval(updateCounter, 1000);
+
+  const changeSize = () => {
+    let cardSize = document.querySelector(".container-card");
+    let inputWidthValue = document.querySelector(".input-width").value;
+    let inputHeightValue = document.querySelector(".input-height").value;
+
+    cardSize.style.width = inputWidthValue + "em";
+    cardSize.style.height = inputHeightValue + "em";
+  };
+
+  document.getElementById("clickSize").addEventListener("click", changeSize);
+};
